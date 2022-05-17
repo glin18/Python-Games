558da11d51d4c5242a61ef4ba9294557374594e0
Gary Lin <garylin2018@gmail.com>
2022-05-17 01:07:26 -0700

    Add random moving cars

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.penup()
+        self.shapesize(stretch_wid=2, stretch_len=1)
+        self.setheading(90)
+        self.random_car()
+
+    def move(self):
+        new_x = self.xcor() - STARTING_MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def random_car(self):
+        y_cor = random.randint(-300, 300)
+        x_cor = 280
+        self.color(random.choice(COLORS))
+        self.goto(x_cor, y_cor)
+
--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -10,11 +10,20 @@ screen.tracer(0)
 screen.title("Turtle Crossing")
 
 player = Player()
+car = CarManager()
+car_list = [car]
 
 screen.listen()
 screen.onkey(player.up, 'Up')
 
 game_is_on = True
+n = 0
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if n % 6 == 0:
+        new_car = CarManager()
+        car_list.append(new_car)
+    for vehicle in car_list:
+        vehicle.move()
+    n += 1
