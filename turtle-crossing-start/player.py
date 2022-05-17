b340b64604a886c433da7baaae74b708ae6548cf
Gary Lin <garylin2018@gmail.com>
2022-05-17 00:50:32 -0700

    Add turtle-crossing-start

---
+++ turtle-crossing-start/player.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.goto(0, -275)
+        self.setheading(90)
+
+    def up(self):
+        new_y = self.ycor() + 10
+        self.goto(self.xcor(), new_y)
